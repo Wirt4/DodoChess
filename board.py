fd1cf756870b4cbd250043b408aa576fee3d3fc9
Wirt4 <wirt.salthouse@gmail.com>
2020-08-14 19:17:31 -0700

    refactored chessboard so not initilizing new objects every screen refresh

--- board.py
+++ board.py
@@ -6,39 +6,41 @@ class Board:
     #need to set up a board of 8 by 8 squares
     #need to create the board in init
     #also will want a draw board method to call in init
-    def __init__(self, dc_game):
+    def __init__(self, dc_game, x_cord, y_cord):
+        self.x_cord = x_cord
+        self.y_cord = y_cord
         self.screen = dc_game.screen
         self.settings = dc_game.settings
         self.color = self.settings.dark_purple
         self.rect = pygame.Rect(0, 0, self.settings.board_width, self.settings.board_width)
         self.screen_rect = self.screen.get_rect()
-        #might need to check below...
-        self.rect.centerx = self.screen_rect.centerx
+        self.squares = []
+        self.create_board(self.settings.board_width)
         #self.rect.midtop = dc_game.screen.midtop
 
     def create_board(self, size):
+        """creates a list of all squares, initilized with appropriate colors and screen coordinates"""
         #link together a bunch of squares in size*size formation
         #white goes in the lower righthand corner
-        pass
-
-    def draw_board(self):
-        """draws the chessboard we created"""
+        length = self.settings.square_size * (self.settings.board_width - 1)
         white = True
-        start_x_coord = self.settings.square_size * self.settings.board_width
-       # y_cord = x_cord
+        start_x_coord = self.x_cord + length
         x_cord = start_x_coord
-        #sets flush  at bottom of square
-        y_cord = self.settings.square_size * (self.settings.board_width-1)
+        y_cord = self.y_cord + length
         for i in range(8):
             for j in range(8):
-                #should get a nice big old board, all yellow
-                square = Square(self.settings, white)
-                square.draw_square(self.screen, x_cord, y_cord)
+                square = Square(self.settings, white, x_cord, y_cord)
+                self.squares.append(square)
                 white = self._toggle_white(white)
                 x_cord -= self.settings.square_size
             x_cord = start_x_coord
             white = self._toggle_white(white)
             y_cord -= self.settings.square_size
+
+    def draw_board(self):
+        """draws the chessboard we created"""
+        for s in self.squares:
+            s.draw_square(self.screen)
 
     def _toggle_white(self, white):
         if white:
--- square.py
+++ square.py
@@ -2,17 +2,17 @@ import pygame
 
 class Square:
     """Class to make a chess square"""
-    def __init__(self, settings, is_white):
+    def __init__(self, settings, is_white, x_cord, y_cord):
         """initializesa assets for square"""
         #self.screen = screen
         self.settings = settings
         self.is_white = is_white
         self.size = self.settings.square_size
         self.my_rect = pygame.Rect(0, 0, self.settings.square_size, self.settings.square_size)
-
-    def draw_square(self, screen, x_cord, y_cord):
         self.my_rect.x = x_cord
         self.my_rect.y = y_cord
+
+    def draw_square(self, screen):
         if self.is_white:
             color = self.settings.white_square
         else:
